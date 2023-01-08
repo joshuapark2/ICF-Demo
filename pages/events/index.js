a7bc83d5bcf1051d3bb35f46c8319f9a9bda427a
Joshua Park <88602267+ironkun1309@users.noreply.github.com>
2023-01-07 19:27:02 -0500

    adding original files for next.js

---
+++ pages/events/index.js
@@ -0,0 +1,25 @@
+const Page = () => {
+    return (
+        <div>
+            <h1> Event Page</h1>
+            <div>
+                <a href=''>
+                    <img />
+                    <h2> Events in London </h2>
+                </a>
+
+                <a href=''>
+                    <img />
+                    <h2> Events in San Francisco </h2>
+                </a>
+
+                <a href=''>
+                    <img />
+                    <h2> Events in Barcelona </h2>
+                </a>
+            </div>
+        </div>
+    )
+};
+
+export default Page;
