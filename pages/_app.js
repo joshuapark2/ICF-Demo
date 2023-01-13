e18ec1228742f5c55c743990bf96b3579504d1f3
Joshua Park <88602267+ironkun1309@users.noreply.github.com>
2023-01-13 04:18:52 -0500

    Finalized

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,6 @@
 import MainLayout from '../src/components/layout/main-layout'
 import '../styles/globals.css'
 import '../styles/generals.sass'
-import '../styles/animations.scss'
 
 export default function App({ Component, pageProps }) {
   return (
