285726bbfbbc82f72990fd973db2dac91b7c3b7a
Joshua Park <88602267+ironkun1309@users.noreply.github.com>
2023-01-13 03:21:26 -0500

    Further SASS fixing and adding formation/Pics for ICF

--- pages/about-us.js
+++ pages/about-us.js
@@ -2,52 +2,94 @@ const AboutUsPage = () => {
     return (
         <div>
             <h1>About us Page</h1>
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et 
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip 
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu 
-                    fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt 
-                    mollit anim id est laborum.
-            </p>
+                <div className="about_us_list">
+                <p>
+                    At ICF International, we are a global consulting and technology services company that provides a wide range 
+                        of services for governments and businesses. Our services include strategic planning, management, marketing, 
+                        and analytics. We have a long history of providing our clients with the highest level of service. 
+                        We were founded in 1969 as the Inner City Fund, with the purpose of financing and helping minority-owned businesses 
+                        win government contracts. As we grew, we reorganized as a consulting firm and renamed ourselves ICF Incorporated. 
+                        We focused on consulting on energy issues for U.S. federal agencies. We then expanded our capabilities by acquiring 
+                        Kaiser Engineering in 1988 and going public in 1989, becoming known as ICF Kaiser. We split in 1999 and in 2006, 
+                        we were renamed ICF International and went public on the NASDAQ. Since then, we have continued to grow through 
+                        acquisitions and expanding our services. 
+                </p>
+                <p>
+                    Our team of professionals is dedicated to providing the highest level of service to our clients. 
+                        We are proud of our ability to tackle a wide range of complex challenges in areas such as energy, 
+                        environment, public health, and economic development. We have been involved in various key developments and events, 
+                        such as the launch of the Coal and Electric Utilities Model, performing environmental and risk assessments for the 
+                        EPA that led to the creation of Superfund, and implementing the Demographic and Health Surveys (DHS) Program. 
+                        We have also worked with HUD and many other organizations. As of 2019, we had US $1.48 billion in revenue with 
+                        approximately 7,000 full and part-time employees in more than 90 offices. We are dedicated to providing the highest 
+                        level of service to our clients, and look forward to working with you.
+                </p>
+                </div>
 
             <h2>
-                Lorem ipsum dolor sit amet
+                Experience Our History
             </h2>
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et 
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip 
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu 
-                    fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt 
-                    mollit anim id est laborum.               
-            </p>
-
-            <ul className="about_us_list">
-                <li>
-                    <h3>
-                        Lorem ipsum dolor sit amet
-                    </h3>
+                <div className="about_us_list">
                     <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et 
-                            dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip 
-                            ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu 
-                            fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt 
-                            mollit anim id est laborum.               
+                        At ICF, we take pride in our rich history that spans over five decades. 
+                            We were founded in 1969 by a former Tuskegee Airman and three U.S. Department of Defense analysts as 
+                            the Inner City Fund, with the mission of financing and supporting minority-owned businesses in Washington D.C. 
+                            As we evolved and grew, we shifted our focus to professional services and consulting, and rebranded as ICF Incorporated.
                     </p>
-                </li>
-
-                <li>
-                    <h3>
-                        Ut enim ad minim veniam
-                    </h3>
                     <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et 
-                            dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip 
-                            ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu 
-                            fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt 
-                            mollit anim id est laborum.               
+                        Throughout our journey, we have been at the forefront of tackling complex challenges in areas such as energy, environment, 
+                            public health, and economic development. Our contributions include being a part of key developments like the launch of the 
+                            Coal and Electric Utilities Model, performing environmental and risk assessments that led to the creation of Superfund, 
+                            and implementing the Demographic and Health Surveys (DHS) Program. We have also had a long-standing partnership with HUD.
                     </p>
-                </li>
-            </ul>
+                    <p>
+                        In 2006, we went public under the stock ticker ICFI, and have continued to expand our capabilities and reach through strategic 
+                            acquisitions. We are proud to have played a significant role in shaping the consulting industry and look forward to continuing 
+                            to make an impact in the years to come.
+                    </p>
+
+                    <ul>
+                        <li>
+                            <h3>
+                                Our Founding
+                            </h3>
+                            <p>
+                                Our founding in 1969 as the Inner City Fund: We, at ICF, were founded in 1969 by a former Tuskegee Airman and 
+                                    three U.S. Department of Defense analysts. Our initial focus was on financing minority-owned businesses in 
+                                    Washington D.C. This was a significant step in our company's history as it was founded with a purpose-driven 
+                                    objective of empowering and providing access to resources for the minority-owned businesses in the country. 
+                                    We are proud of our roots and continue to strive for inclusivity and diversity in all our endeavors.            
+                            </p>
+                        </li>
+
+                        <li>
+                            <h3>
+                                Transition from Venture Capital Firm to Consulting Firm
+                            </h3>
+                            <p>
+                                As we progressed and grew in the 1970s and 1980s, we made a significant shift in our focus from venture capital to 
+                                    professional services and consulting. We became a trusted partner in policy economics and strategy, 
+                                    specializing in areas such as energy, environment, public health, and economic development. 
+                                    This shift marked a significant change in our operations and enabled us to provide more value to our clients, 
+                                    ultimately driving our growth as a company.          
+                            </p>
+                        </li>
+
+                        <li>
+                            <h3>
+                                Our Growth and Expansion
+                            </h3>
+                            <p>
+                                Our growth and expansion through a series of strategic acquisitions in the 2000s and 2010s has 
+                                    allowed us to expand our services and reach a global audience. We have acquired companies such as 
+                                    London-based GHK Holding Limited, a consulting agency working with government and commercial clients, 
+                                    which has enabled us to establish a strong presence in the international market and establish ourselves as a 
+                                    leader in the industry. Going public in 2006 and accessing more capital through subsequent acquisitions has allowed 
+                                    us to continue our growth and further solidify our position as a leading consulting firm.    
+                            </p>
+                        </li>
+                    </ul>
+                </div>
         </div>
     )
 }
